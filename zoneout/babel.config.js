16999b6b293c05f904591b3a967450b9c6f8e422
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2024-11-03 16:59:35 +0530

    missing parent added

---
+++ zoneout/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        blocklist: null,
+        allowlist: null,
+        verbose: false,
+      },
+    ],
+  ],
+};
---
+++ zoneout/src/services/socketio.ts
@@ -0,0 +1,2 @@
+import { io } from "socket.io-client";
+export const socket = io("http://172.20.10.4:3001");
